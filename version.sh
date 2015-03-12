cfd8c586682958d5f9562f0e24a14699716421b0
Lloydangifer <vmanrique@netcourrier.com>
2015-03-12 11:03:08 +0100

    exercise 2 done. cookies.txt is a test file

--- version.sh
+++ version.sh
@@ -4,7 +4,19 @@ if [ $# -ne 2 ]
 then
     echo 'Usage: version.sh <cmd> <file> [option]'
     echo 'where <cmd> can be: add checkout commit diff log revert rm'
-elif [ -e $2 ] 
+    exit
+elif [ ! -e $2 ] 
 then
-    echo 'Error! ’$2.txt’ is not a file.'
+    echo 'Error!' $2 'is not a file.'
+    exit
 fi
+case $1 in
+        add) echo 'Added a new file under versioning: '$2'';;
+	checkout) echo 'checkout test';;
+	commit) echo 'commit test';;
+	diff) echo 'diff test';;
+	log) echo 'log test';;
+	revert) echo 'revert test';;
+	rm) echo 'rm test';;
+	*) echo 'Error! This command name does not exist: ’'$1'’';;
+esac
